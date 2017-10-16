ae771d8fa0b4fc08f844c10b75ef559d1a04054b
Vadim Pasternak <vadimp@mellanox.com>
2017-10-16 18:14:33 +0300

    Update helper scripts
    Signed-off-by: Vadim Pasternak <vadimp@mellanox.com>

--- usr/usr/bin/mellanox-system-helper.sh
+++ usr/usr/bin/mellanox-system-helper.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-if [ "$1" == "cpld" ]; then
-	if [ "$2" == "mgmt" ]; then
+if [ "$0" == "/bsp/cpld" ]; then
+	if [ "$1" == "mgmt" ]; then
 		/usr/sbin/iorw -b 0x2500 -r -l1 | awk '{print $5}'
-	elif [ "$2" == "brd" ]; then
+	elif [ "$1" == "brd" ]; then
 		/usr/sbin/iorw -b 0x2501 -r -l1 | awk '{print $5}'
-	elif [ "$2" == "brd" ]; then
+	elif [ "$1" == "port" ]; then
 		sudo /usr/bin/mlxreg -d /dev/mst/mt52100_pciconf0 --get --reg_id 0x902A --reg_len 16 | tail -n4 | head -n1 | awk '{print $3}'
 	fi
 fi
 
-if [ "$1" == "system" ]; then
-	if [ "$2" == "mgmt" ]; then
-		/usr/sbin/iorw -b 0x2500 -r -l1 | awk '{print $5}'
-	elif [ "$2" == "brd" ]; then
-		/usr/sbin/iorw -b 0x2501 -r -l1 | awk '{print $5}'
-	elif [ "$2" == "brd" ]; then
-		sudo /usr/bin/mlxreg -d /dev/mst/mt52100_pciconf0 --get --reg_id 0x902A --reg_len 16 | tail -n4 | head -n1 | awk '{print $3}'
+if [ "$0" == "/bsp/system" ]; then
+	if [ "$1" == "pwr_cycle" ]; then
+		/usr/sbin/iorw -b 0x2531 -w -l1 -v0x00
+		/usr/sbin/iorw -b 0x2530 -w -l1 -v0x04
 	fi
 fi
+
+if [ "$0" == "/bsp/pwr_consum" ]; then
+	/usr/sbin/iorw -b 0x2533 -w -l1 -v0xbf
+	regval=`/usr/sbin/iorw -b 0x2532 -r -l1 | awk '{print $5}'`
+
+	if [ "$1" == "psu1" ]; then
+		regnew=`echo $(($regval | 0x40))`
+	elif [ "$1" == "psu2" ]; then
+		regnew=`echo $(($regval & 0xbf))`
+	fi
+
+	/usr/sbin/iorw -b 0x2532 -w -l1 -v$regnew
+	iioreg=`cat /bsp/environment/a2d_iio\:device0_raw_1`
+	echo $(($iioreg * 80 * 12))
+fi
+
