70443994165055c9dcd2491dfc909c3ac55b68ac
Vadim Pasternak <vadimp@mellanox.com>
2018-11-12 11:56:04 +0200

    Update scripts

--- usr/usr/bin/mellanox-system-event.sh
+++ usr/usr/bin/mellanox-system-event.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-########################################################################
-# Copyright (c) 2017 Mellanox Technologies.
-# Copyright (c) 2017 Vadim Pasternak <vadimp@mellanox.com>
+# Copyright (c) 2018 Mellanox Technologies. All rights reserved.
 #
-# Licensed under the GNU General Public License Version 2
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the names of the copyright holders nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
 #
+# Alternatively, this software may be distributed under the terms of the
+# GNU General Public License ("GPL") version 2 as published by the Free
+# Software Foundation.
 #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
 fan_command=0x3b
 fan_psu_default=0x3c
 i2c_bus_max=10
@@ -313,7 +332,6 @@ if [ "$1" == "add" ]; then
     fi
   fi
 elif [ "$1" == "change" ]; then
-    echo "Do nothing on change"
 	if [ "$2" == "hotplug_asic" ]; then
 		if [ -d /sys/module/mlxsw_pci ]; then
 			return
@@ -321,7 +339,6 @@ elif [ "$1" == "change" ]; then
 		find_i2c_bus
 		bus=$(($i2c_asic_bus_default+$i2c_bus_offset))
 		path=/sys/bus/i2c/devices/i2c-$bus
-		echo $2 $3 $4 $5 $6 $7 $8 $9 $path >> /tmp/trace
 		if [ "$3" == "up" ]; then
 			if [ ! -d /sys/module/mlxsw_minimal ]; then
 				modprobe mlxsw_minimal
