3835ddac7f71dfdd2ec0b90aa4521c5ae73d5615
Oleksandr Shamray <oleksandrs@mellanox.com>
2019-05-22 13:46:28 +0000

    Fix docker build script to run from Jenkins

--- docker_build/build.sh
+++ docker_build/build.sh
@@ -7,6 +7,6 @@ if [ ! -d "$OUTPUT" ]; then
     mkdir $OUTPUT
 fi
 docker build -t docker-deb-builder:stretsh -f ${PWD}/dockerfile_hw_mgmt ${PWD}/
-docker run -it -v ${PWD}/../:/source-ro:ro -v ${OUTPUT}:/output -v ${PWD}/build-helper.sh:/build-helper.sh:ro -e USER=5616 -e GROUP=101 --rm  docker-deb-builder:stretsh /build-helper.sh
+docker run -v ${PWD}/../:/source-ro:ro -v ${OUTPUT}:/output -v ${PWD}/build-helper.sh:/build-helper.sh:ro -e USER=5616 -e GROUP=101 --rm  docker-deb-builder:stretsh /build-helper.sh
 cd ${OUTPUT}
 echo $(pwd)/$(ls)
