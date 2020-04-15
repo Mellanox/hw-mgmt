306c18192ee1d9bf4f009e646f305c20bf2ab7bb
Vadim Pasternak <vadimp@mellanox.com>
2020-04-15 12:22:10 +0300

    hw-mgmt: service: Fix flow for adding PSU EEPROM
    Fix flow for attaching PSU EEPROM driver - make it only after
    /var/run/hw-management/config folder is available.
    
    Remove "ExecStartPost" from systemd service, call it directly instead.
    Using this service could make a conflict for users which disable
    hw-management thermal control, which creates process 'PID'. For such
    users 'PID' will not be created and "ExecStartPost" will fail.
    
    Signed-off-by: Vadim Pasternak <vadimp@mellanox.com>

--- usr/usr/bin/hw-management-start-post.sh
+++ usr/usr/bin/hw-management-start-post.sh
@@ -31,19 +31,15 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-
-# ww-management command that is executed after hw-management start.
-# Activated by systemd.
+# hw-management script that is executed at the end of hw-management start.
 
 # Local constants and paths.
-wait_pid_cntr=3
 max_cpld=4
 max_fan_drwr=8
 hw_management_path=/var/run/hw-management
 config_path=$hw_management_path/config
 system_path=$hw_management_path/system
 thermal_path=/var/run/hw-management/thermal
-PID=/var/run/hw-management.pid
  
 handle_cpld_versions()
 {
@@ -90,12 +86,6 @@ set_fan_drwr_num()
 	done
 	echo $drwr_num > $config_path/fan_drwr_num
 }
-
-# Wait for PID.
-while [ ! -f $PID ] && [ "$wait_pid_cntr" -gt  "0" ]; do
-	sleep 1
-	wait_pid_cntr=$(($wait_pid_cntr-11))
-done
 
 board=`cat /sys/devices/virtual/dmi/id/board_name`
 cpld_num=`cat $config_path/cpld_num`
