f575b534217d4a41badb8795c1b93f56520b4fc2
sholeksandr <oleksandrs@nvidia.com>
2021-04-29 13:56:14 +0300

    Update hw-management-generate-dump.sh

--- usr/usr/bin/hw-management-generate-dump.sh
+++ usr/usr/bin/hw-management-generate-dump.sh
@@ -46,6 +46,7 @@ dump_cmd () {
 
 	if [ -x "$(command -v $cmd_name)" ];
 	then
+		# ignore shellcheck message SC2016. Arguments should be single-quoted (')
 		eval '$cmd' '&>' '$DUMP_FOLDER/$output_fname'
 	fi
 }
