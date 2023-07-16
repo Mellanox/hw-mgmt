2d80b5969fa6386ec8e1c38fe6530399bb6329d2
Vadim Pasternak <vadimp@nvidia.com>
2023-07-16 18:37:44 +0000

    hw-mgmt: kexec notifier: Change setting of kexec attribute
    Inverse setting from 1 to 0.
    
    Signed-off-by: Vadim Pasternak <vadimp@nvidia.com>

--- usr/usr/bin/hw-management-kexec-notifier.sh
+++ usr/usr/bin/hw-management-kexec-notifier.sh
@@ -3,6 +3,6 @@
 PLAT_KEXEC_NOTIFY=/var/run/hw-management/system/kexec_activated
 
 if [ "$1" = "kexec" ] && [ -f ${PLAT_KEXEC_NOTIFY} ]; then
-        echo 1 > ${PLAT_KEXEC_NOTIFY}
+        echo 0 > ${PLAT_KEXEC_NOTIFY}
 fi
 
