3c300c3eb18caf83674df7bf30d63eefc826daee
Felix Radensky <fradensky@nvidia.com>
2024-04-09 20:02:57 +0300

    hw-mgmt: scripts: Fix ASIC PCIE mapping for QM3000
    Signed-off-by: Felix Radensky <fradensky@nvidia.com>

--- usr/usr/bin/hw-management.sh
+++ usr/usr/bin/hw-management.sh
@@ -2703,9 +2703,9 @@ set_asic_pci_id()
 		echo -n "$asics" | grep -c '^' > "$config_path"/asic_num
 		[ -z "$asics" ] && return
 		asic1_pci_bus_id=`echo $asics | awk '{print $3}'`
-		asic2_pci_bus_id=`echo $asics | awk '{print $1}'`
-		asic3_pci_bus_id=`echo $asics | awk '{print $4}'`
-		asic4_pci_bus_id=`echo $asics | awk '{print $2}'`
+		asic2_pci_bus_id=`echo $asics | awk '{print $2}'`
+		asic3_pci_bus_id=`echo $asics | awk '{print $1}'`
+		asic4_pci_bus_id=`echo $asics | awk '{print $4}'`
 		echo "$asic1_pci_bus_id" > "$config_path"/asic1_pci_bus_id
 		echo "$asic2_pci_bus_id" > "$config_path"/asic2_pci_bus_id
 		echo "$asic3_pci_bus_id" > "$config_path"/asic3_pci_bus_id
