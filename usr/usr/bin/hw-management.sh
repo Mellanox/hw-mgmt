f5bca96eb6934d1b5d5e11729bcdf67117887334
Oleksandr Shamray <oleksandrs@nvidia.com>
2024-08-20 14:00:56 +0300

    hw-mgmt: topology: Add N5101_LD MSFT support
    Add n5101_LD MSFT support HW_ID:170, SKU:HI170, VMOD0021
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw-management.sh
+++ usr/usr/bin/hw-management.sh
@@ -2232,7 +2232,7 @@ n51xxld_specific()
 			echo 6 > $config_path/fan_drwr_num
 			thermal_control_config="$thermal_control_configs_path/tc_config_n5110ld.json"
 		;;
-		HI166|HI169)	# TTM
+		HI166|HI169|HI170)	# TTM, ARIEL, MSFT
 			max_tachos=8
 			echo 4 > $config_path/fan_drwr_num
 			thermal_control_config="$thermal_control_configs_path/tc_config_n5110ld_ttm.json"
@@ -2537,7 +2537,7 @@ load_modules()
 	esac
 
 	case $sku in
-		HI162|HI166|HI167|HI169)	# Juliet
+		HI162|HI166|HI167|HI169|HI170)	# Juliet
 			modprobe i2c_asf
 			modprobe i2c_designware_platform
 		;;
@@ -2767,7 +2767,7 @@ set_asic_pci_id()
 			asic_pci_id=$nv4_rev_a1_pci_id
 		fi
 		;;
-	HI157|HI162|HI166|HI167|HI169)
+	HI157|HI162|HI166|HI167|HI169|HI170)
 		asic_pci_id=${quantum3_pci_id}
 		;;
 	HI158)
@@ -2788,7 +2788,7 @@ set_asic_pci_id()
 		echo "$asic2_pci_bus_id" > "$config_path"/asic2_pci_bus_id
 		echo 2 > "$config_path"/asic_num
 		;;
-	HI131|HI141|HI142|HI152|HI162|HI166|HI167|HI169)
+	HI131|HI141|HI142|HI152|HI162|HI166|HI167|HI169|HI170)
 		asic1_pci_bus_id=`echo $asics | awk '{print $1}'`
 		asic2_pci_bus_id=`echo $asics | awk '{print $2}'`
 		echo "$asic1_pci_bus_id" > "$config_path"/asic1_pci_bus_id
@@ -2984,7 +2984,7 @@ pre_devtr_init()
 		;;
 	VMOD0021)
 		case $sku in
-		HI162|HI166|HI167|HI169)
+		HI162|HI166|HI167|HI169|HI170)
 			echo 55 > $config_path/cpu_brd_bus_offset
 			;;
 		*)
