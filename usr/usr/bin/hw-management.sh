29b5305869b9f5671617f53ba1d2cc32c8f2a775
Oleksandr Shamray <oleksandrs@nvidia.com>
2024-10-28 11:19:00 +0200

    hw-mgmt: topology: Update N5200_LD no NCI platform
    Changew SKU for 5200_LD no NCI to 167
    SKU :173 -> 167
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw-management.sh
+++ usr/usr/bin/hw-management.sh
@@ -2232,20 +2232,15 @@ n51xxld_specific()
 			echo 6 > $config_path/fan_drwr_num
 			thermal_control_config="$thermal_control_configs_path/tc_config_n5110ld.json"
 		;;
-		HI166|HI169|HI170)	# TTM, ARIEL, MSFT
+		HI166|HI169)	# TTM, ARIEL
 			max_tachos=8
 			echo 4 > $config_path/fan_drwr_num
 			thermal_control_config="$thermal_control_configs_path/tc_config_n5110ld_ttm.json"
 		;;
-		HI167)	# NSO
+		HI167|HI170)	# NSO, NSO no NCI, DGX, MSFT
 			max_tachos=8
 			echo 4 > $config_path/fan_drwr_num
 			thermal_control_config="$thermal_control_configs_path/tc_config_n5100ld.json"
-		;;
-		HI173)	# NSO no NCI
-			max_tachos=8
-			echo 4 > $config_path/fan_drwr_num
-			thermal_control_config="$thermal_control_configs_path/tc_config_n5200ld.json"
 		;;
 		*)
 			max_tachos=8
@@ -2542,7 +2537,7 @@ load_modules()
 	esac
 
 	case $sku in
-		HI162|HI166|HI167|HI169|HI170|HI173)	# Juliet
+		HI162|HI166|HI167|HI169|HI170)	# Juliet
 			modprobe i2c_asf
 			modprobe i2c_designware_platform
 		;;
@@ -2772,7 +2767,7 @@ set_asic_pci_id()
 			asic_pci_id=$nv4_rev_a1_pci_id
 		fi
 		;;
-	HI157|HI162|HI166|HI167|HI169|HI170|HI173)
+	HI157|HI162|HI166|HI167|HI169|HI170)
 		asic_pci_id=${quantum3_pci_id}
 		;;
 	HI158)
@@ -2793,7 +2788,7 @@ set_asic_pci_id()
 		echo "$asic2_pci_bus_id" > "$config_path"/asic2_pci_bus_id
 		echo 2 > "$config_path"/asic_num
 		;;
-	HI131|HI141|HI142|HI152|HI162|HI166|HI167|HI169|HI170|HI173)
+	HI131|HI141|HI142|HI152|HI162|HI166|HI167|HI169|HI170)
 		asic1_pci_bus_id=`echo $asics | awk '{print $1}'`
 		asic2_pci_bus_id=`echo $asics | awk '{print $2}'`
 		echo "$asic1_pci_bus_id" > "$config_path"/asic1_pci_bus_id
@@ -2989,7 +2984,7 @@ pre_devtr_init()
 		;;
 	VMOD0021)
 		case $sku in
-		HI162|HI166|HI167|HI169|HI170|HI173)
+		HI162|HI166|HI167|HI169|HI170)
 			echo 55 > $config_path/cpu_brd_bus_offset
 			;;
 		*)
