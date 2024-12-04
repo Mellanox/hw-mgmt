04d7ac1fd3515ca3d5c9821e8431da1733d95c54
Oleksandr Shamray <oleksandrs@nvidia.com>
2024-12-04 12:05:34 +0200

    hw-mgmt: attributes: Fix module temp input name in sync script
    Change moduleX temperature input attribute name:
    
    moduleXX -> module XX_temp_input
    
    Bug: 4198371
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw_management_sync.py
+++ usr/usr/bin/hw_management_sync.py
@@ -674,6 +674,10 @@ def module_temp_populate(arg_list, arg):
     """
     @summary: Update module attributes
     """
+    f_name = "/var/run/hw-management/thermal/{}_temp_input".format(arg_list[0])
+    if os.path.islink(f_name):
+        return
+
     try:
         arg = int(arg)
         if arg >= 0:
@@ -690,8 +694,7 @@ def module_temp_populate(arg_list, arg):
         temp_emergency = ""
         temp_fault = ""
         temp_trip_crit = ""
-        
-    f_name = "/var/run/hw-management/thermal/{}_temp_input".format(arg_list[0])
+
     with open(f_name, 'w', encoding="utf-8") as f:
         f.write(str(val)+"\n")
 
