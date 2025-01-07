5ffe7c49c90b7a217fcf0aaebf7412b8507834fd
Oleksandr Shamray <oleksandrs@nvidia.com>
2025-01-07 23:37:47 +0200

    hw-mgmt: thermal: Add PCH thermal sensor support
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw_management_thermal_control.py
+++ usr/usr/bin/hw_management_thermal_control.py
@@ -258,9 +258,9 @@ SENSOR_DEF_CONFIG = {
                          "input_suffix": "_input", "input_smooth_level": 2
                         },
     r'pch':             {"type": "thermal_sensor",
-                         "pwm_min": 30, "pwm_max": 60, "val_min": 50000, "val_max": 85000,
-                         "val_lcrit": 0, "val_hcrit": 150000, "poll_time": 10,
-                         "input_suffix": "_temp", "value_hyst": 2, "input_smooth_level": 2, "enable": 0
+                         "pwm_min": 30, "pwm_max": 100, "val_min": 70000, "val_max": 108000,
+                         "val_lcrit": 0, "val_hcrit": 150000, "poll_time": 3,
+                         "input_suffix": "_temp", "value_hyst": 2, "input_smooth_level": 3, "enable": 0
                         },
     r'comex_amb':       {"type": "thermal_sensor",
                          "pwm_min": 30, "pwm_max": 60, "val_min": 45000, "val_max": 85000, "value_hyst": 2, "poll_time": 3, "enable": 0
@@ -2595,6 +2595,7 @@ class ThermalManagement(hw_managemet_file_op):
                           r'sodimm\d+':"add_sodimm_sensor",
                           r'sensor_amb':"add_amb_sensor",
                           r'drivetemp':"add_drivetemp_sensor",
+                          r'pch':"add_pch_sensor",
                           r'ibc\d*':"add_ibc_sensor",
                           r'pdb_pwr\d*':"add_pdb_pwr_sensor",
                           r'ctx_amb\d*':"add_connectx_sensor",
@@ -3369,6 +3370,11 @@ class ThermalManagement(hw_managemet_file_op):
 
     # ----------------------------------------------------------------------
     def add_drivetemp_sensor(self, name):
+        in_file = "thermal/{}".format(name)
+        self._sensor_add_config("thermal_sensor", name, {"base_file_name": in_file})
+
+    # ----------------------------------------------------------------------
+    def add_pch_sensor(self, name):
         in_file = "thermal/{}".format(name)
         self._sensor_add_config("thermal_sensor", name, {"base_file_name": in_file})
 
