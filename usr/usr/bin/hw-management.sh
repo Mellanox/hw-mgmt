2cee1a24baee43771d771a29069684861189f654
Felix Radensky <fradensky@nvidia.com>
2025-02-23 19:44:01 +0200

    hw-mgmt: scripts: Disable minimal driver on SN4600/SN4600C/SN4410/SN4700/SN4280 systems
    Signed-off-by: Felix Radensky <fradensky@nvidia.com>

--- usr/usr/bin/hw-management.sh
+++ usr/usr/bin/hw-management.sh
@@ -1556,6 +1556,7 @@ msn47xx_specific()
 	fi
 
 	max_tachos=12
+	minimal_unsupported=1
 	echo 25000 > $config_path/fan_max_speed
 	echo 4500 > $config_path/fan_min_speed
 	echo 23000 > $config_path/psu_fan_max
@@ -1603,6 +1604,7 @@ msn46xx_specific()
 
 	max_tachos=3
 	hotplug_fans=3
+	minimal_unsupported=1
 	echo 23000 > $config_path/psu_fan_max
 	echo 4600 > $config_path/psu_fan_min
 	echo 3 > $config_path/cpld_num
@@ -2316,6 +2318,7 @@ smart_switch_common()
 	named_busses+=(${smart_switch_named_busses[@]})
 	echo -n "${named_busses[@]}" > $config_path/named_busses
 	max_tachos=4
+	minimal_unsupported=1
 	echo 11000 > $config_path/fan_max_speed
 	echo 3100 > $config_path/fan_min_speed
 	echo 23000 > $config_path/psu_fan_max
