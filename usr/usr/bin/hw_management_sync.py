c102e9fb33a5347deecca7edd90f1dce4da9193f
Felix Radensky <fradensky@nvidia.com>
2025-03-05 11:15:52 +0200

    hw-mgmt: scripts: Add Q3451 XDR platform support to hw-mgmt sync service
    Signed-off-by: Felix Radensky <fradensky@nvidia.com>

--- usr/usr/bin/hw_management_sync.py
+++ usr/usr/bin/hw_management_sync.py
@@ -226,6 +226,16 @@ atttrib_list = {
          "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 91}
         }
     ],
+    "HI178": [
+       {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
+         "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic1": {"fin": "/sys/module/sx_core/asic0/"},
+                 }
+        },
+        {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
+         "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 24}
+        }
+    ],
     "def": [
          {"fin": "/var/run/hw-management/config/thermal_enforced_full_spped",
          "fn": "run_cmd",
