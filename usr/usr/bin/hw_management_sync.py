b102478fab3ee639e8a998c5ccbd061f3c2124c1
Oleksandr Shamray <oleksandrs@nvidia.com>
2025-03-12 10:44:46 +0200

    hw-mgmgt: topo: Fix module/asic count for HI176/177 systems
    Fix module/asic count for HI176/177 systems
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw_management_sync.py
+++ usr/usr/bin/hw_management_sync.py
@@ -160,6 +160,37 @@ atttrib_list = {
          "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 66}
         }        
     ],
+    "HI176": [
+        {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
+         "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic1": {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic2": {"fin": "/sys/module/sx_core/asic1/"}
+                }
+        },
+
+        {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
+         "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 36}
+        },
+        
+        {"fin": None,
+         "fn": "redfish_get_sensor", "arg" : ["/redfish/v1/Chassis/MGX_BMC_0/Sensors/BMC_TEMP", "bmc", 1000], "poll": 30, "ts": 0}     
+    ],
+    "HI177": [
+        {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
+         "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic1": {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic2": {"fin": "/sys/module/sx_core/asic1/"},
+                    "asic3": {"fin": "/sys/module/sx_core/asic3/"}
+                }
+        },
+
+        {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
+         "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 54}
+        },
+        
+        {"fin": None,
+         "fn": "redfish_get_sensor", "arg" : ["/redfish/v1/Chassis/MGX_BMC_0/Sensors/BMC_TEMP", "bmc", 1000], "poll": 30, "ts": 0}   
+    ],
     "def": [
          {"fin": "/var/run/hw-management/config/thermal_enforced_full_spped",
          "fn": "run_cmd",
