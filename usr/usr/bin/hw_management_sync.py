baaba8c7461bfecb054ae833254a6cd7ce42a482
Felix Radensky <fradensky@nvidia.com>
2025-03-30 15:02:57 +0300

    hw-mgmt: scripts: Disable minimal driver on SN3420/SN3510/SN3700/SN3700C/SN3750SX systems
    Signed-off-by: Felix Radensky <fradensky@nvidia.com>

--- usr/usr/bin/hw_management_sync.py
+++ usr/usr/bin/hw_management_sync.py
@@ -161,6 +161,36 @@ atttrib_list = {
          "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 66}
         }        
     ],
+    "HI112|HI116|HI136": [
+        {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
+         "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic1": {"fin": "/sys/module/sx_core/asic0/"}
+                }
+        },
+        {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
+         "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 32}
+        }
+    ],
+    "HI120": [
+        {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
+         "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic1": {"fin": "/sys/module/sx_core/asic0/"}
+                }
+        },
+        {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
+         "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 60}
+        }
+    ],
+    "HI121": [
+        {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
+         "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic1": {"fin": "/sys/module/sx_core/asic0/"}
+                }
+        },
+        {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
+         "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 54}
+        }
+    ],
     "HI122": [
         {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
          "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
@@ -179,6 +209,16 @@ atttrib_list = {
         },
         {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
          "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 64}
+        }
+    ],
+    "HI146": [
+        {"fin": None, "fn": "asic_temp_populate", "poll": 3, "ts": 0,
+         "arg" : {  "asic":  {"fin": "/sys/module/sx_core/asic0/"},
+                    "asic1": {"fin": "/sys/module/sx_core/asic0/"}
+                }
+        },
+        {"fin": None, "fn": "module_temp_populate", "poll": 20, "ts": 0,
+         "arg" : {"fin": "/sys/module/sx_core/asic0/module{}/", "fout_idx_offset": 1, "module_count": 32}
         }
     ],
     "HI160": [
