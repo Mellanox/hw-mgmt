9e6379e6f46dfcf02c5f7f2507cf7200c5ba6167
Oleksandr Shamray <oleksandrs@nvidia.com>
2025-07-10 17:30:57 +0300

    hw-mgmt: thermal: Add fan state validation after re-insertion
    [Backport to TC 2.0]
    
    A customer (case #4486661) reported an issue:
    
    After reinserting fans in some systems, a fan may fail to start
    rotating.
    A stuck fan can start rotating by either of the following actions:
    
    1) Pulling out and reinserting an additional fan.
    2) Lowering the PWM of all fans for several seconds.
    
    This behavior occurs because the reinserted fan sometimes fails to
    generate enough starting torque to overcome the opposing air pressure
    generated by operating fans.
    
    This commit introduces 3 configurable options in tc_config.json:
    
    1. fan_steady_state_delay:
    The delay in seconds during which thermal control (TC) is paused after a
    corner fan is inserted.
    Default: 10 seconds.
    
    2. fan_steady_state_pwm:
    The PWM value (in percentage) to apply for the duration of
    fan_steady_state_delay after an attention fan is inserted.
    PWM will be restored to its previous value after the delay.
    Default: 50%.
    
    3. attention_fans:
    A list of fans that require special handling after insertion (monitoring
    state and restart if needed).
    
    TC will monitor the state of each fan after reinsertion and trigger
    recovery logic if a fan fails to start within 5 seconds.
    
    Thermal Control 2.5 has been updated to support these new configuration
    options.
    It adds logic to detect attention fan re-insertion and adjust the PWM
    accordingly.
    
    Example tc_config.json:
    "general_config": {
        "attention_fans": ["drwr1", "drwr5"],
        "fan_steady_state_delay": 10,
        "fan_steady_state_pwm": 50
    }
    
    FR: #4521169
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw_management_thermal_control.py
+++ usr/usr/bin/hw_management_thermal_control.py
@@ -96,6 +96,10 @@ class CONST(object):
     SYS_CONF_ERR_MASK = "error_mask"
     SYS_CONF_REDUNDANCY_PARAM = "redundancy"
     SYS_CONF_GENERAL_CONFIG_PARAM = "general_config"
+    SYS_CONF_FAN_STEADY_STATE_DELAY = "fan_steady_state_delay"
+    SYS_CONF_FAN_STEADY_STATE_PWM = "fan_steady_state_pwm"
+    SYS_CONF_FAN_STEADY_ATTENTION_ITEMS = "attention_fans"
+
 
     # *************************
     # Folders definition
@@ -202,6 +206,10 @@ class CONST(object):
 
     MLXREG_SET_CMD_STR = "yes |  mlxreg -d  {pcidev} --reg_name MFSC --indexes \"pwm=0x0\" --set \"pwm_duty_cycle={pwm}\""
     MLXREG_GET_CMD_STR = "mlxreg -d {pcidev} --reg_name MFSC --get --indexes \"pwm=0x0\" | grep pwm | head -n 1 | cut -d '|' -f 2"
+
+    # attention fan insertion recovery defaults
+    FAN_STEADY_STATE_DELAY_DEF = 0
+    FAN_STEADY_STATE_PWM_DEF = 50
 
 """
 Default sensor  configuration.
@@ -2032,6 +2040,11 @@ class fan_sensor(system_device):
 
         self.rpm_valid_state = True
 
+        self.insert_status = 0
+        self.insert_event_ts = 0
+        self.insert_failed = False
+        self.insert_event = False
+
     # ----------------------------------------------------------------------
     def sensor_configure(self):
         """
@@ -2049,6 +2062,11 @@ class fan_sensor(system_device):
         self.drwr_param = self._get_fan_drwr_param()
         self.fan_shutdown(False)
         self.pwm_set = self.read_pwm(CONST.PWM_MIN)
+
+        self.insert_status = 0
+        self.insert_event_ts = 0
+        self.insert_failed = False
+        self.insert_event = False
 
     # ----------------------------------------------------------------------
     def refresh_attr(self):
@@ -2112,6 +2130,42 @@ class fan_sensor(system_device):
             except BaseException:
                 self.log.error("Value reading from file: {}".format(status_filename))
         return status
+
+    # ----------------------------------------------------------------------
+    def update_insert_state(self):
+        """
+        @summary: Update insert state
+        """
+        status = self._get_status()
+        if status:
+            if not self.insert_status:
+                self.insert_event_ts = self.get_timestump()
+                self.insert_event = True
+        else:
+            self.insert_event = 0
+            self.insert_failed = False
+        self.insert_status = status
+
+    # ----------------------------------------------------------------------
+    def is_insert_failed(self):
+        """
+        @summary: Check if insert failed
+        """
+        if self.insert_event:
+            if self.insert_event_ts + CONST.FAN_RELAX_TIME * 1000 <= self.get_timestump():
+                fan_fault_list = self._get_fault()
+                if any(x == 1 for x in fan_fault_list):
+                    self.insert_failed = True
+                self.insert_event = False
+
+        return self.insert_failed
+
+    # ----------------------------------------------------------------------
+    def reset_insert_failed_state(self):
+        """
+        @summary: Reset insert failed state
+        """
+        self.insert_failed = False
 
     # ----------------------------------------------------------------------
     def _get_fault(self):
@@ -2684,6 +2738,18 @@ class ThermalManagement(hw_managemet_file_op):
                 self.exit.wait(10)
             self.log.notice("PWM control activated", 1)
 
+        self.attention_fans_lst = get_dict_val_by_path(self.sys_config, [CONST.SYS_CONF_GENERAL_CONFIG_PARAM, CONST.SYS_CONF_FAN_STEADY_ATTENTION_ITEMS])
+        if self.attention_fans_lst:
+            self.fan_steady_state_delay = get_dict_val_by_path(self.sys_config, [CONST.SYS_CONF_GENERAL_CONFIG_PARAM, CONST.SYS_CONF_FAN_STEADY_STATE_DELAY])
+            if not self.fan_steady_state_delay:
+                self.fan_steady_state_delay = CONST.FAN_STEADY_STATE_DELAY_DEF
+            self.fan_steady_state_pwm = get_dict_val_by_path(self.sys_config, [CONST.SYS_CONF_GENERAL_CONFIG_PARAM, CONST.SYS_CONF_FAN_STEADY_STATE_PWM])
+            if not self.fan_steady_state_pwm:
+                self.fan_steady_state_delay = CONST.FAN_STEADY_STATE_PWM_DEF
+            self.log.info("Fan {} insertion recovery enabled: delay {}s, pwm {}%".format(self.attention_fans_lst,
+                                                                                         self.fan_steady_state_delay,
+                                                                                         self.fan_steady_state_pwm))
+
         # Set PWM to the default state while we are waiting for system configuration
         self.log.notice("Set FAN PWM {}".format(self.pwm_target), 1)
         if not self.write_pwm(self.pwm_target, validate=True):
@@ -2933,6 +2999,30 @@ class ThermalManagement(hw_managemet_file_op):
             pref_dir = CONST.P2C
 
         return pref_dir
+
+    # ---------------------------------------------------------------------
+    def _is_attention_fan_insertion_fail(self):
+        fan_insert_failed = False
+        for fan_obj in self.attention_fans:
+            fan_obj.update_insert_state()
+            if fan_obj.is_insert_failed():
+                self.log.notice("{} fan not started after insertion".format(fan_obj.name))
+                fan_obj.reset_insert_failed_state()
+                fan_insert_failed = True
+                break
+        return fan_insert_failed
+
+    # ---------------------------------------------------------------------
+    def _attention_fan_insertion_recovery(self):
+        pwm = self.read_pwm(100)
+        self.log.notice("Attention fan not started after insertion: Setting pwm to {}% from {}%".format(self.fan_steady_state_pwm, pwm), 1)
+        self._update_chassis_fan_speed(self.fan_steady_state_pwm, force=True)
+        self.log.info("Waiting {}s for newly inserted fan to stabilize".format(self.fan_steady_state_delay))
+        timeout = current_milli_time() + 1000 * self.fan_steady_state_delay
+        while timeout > current_milli_time():
+            self.exit.wait(1)
+        self.log.info("Resuming normal operation: Setting pwm back to {}%".format(pwm))
+        self._update_chassis_fan_speed(pwm, force=True)
 
     # ----------------------------------------------------------------------
     def _update_psu_fan_speed(self, pwm):
@@ -3520,6 +3610,16 @@ class ThermalManagement(hw_managemet_file_op):
         self.dev_obj_list.sort(key=lambda x: x.name)
         self.write_file(CONST.PERIODIC_REPORT_FILE, self.periodic_report_time)
 
+        self.attention_fans = []
+        if self.attention_fans_lst:
+            for fan_drwr_name in self.attention_fans_lst:
+                fan_drwr_obj = self._get_dev_obj(fan_drwr_name)
+                if not fan_drwr_obj:
+                    self.log.warn("Dev name {} missing in system_config".format(fan_drwr_name))
+                    continue
+                self.attention_fans.append(fan_drwr_obj)
+                self.log.info("{} added to attention_fans".format(fan_drwr_name))
+
     # ----------------------------------------------------------------------
     def start(self, reason=""):
         """
@@ -3623,6 +3723,11 @@ class ThermalManagement(hw_managemet_file_op):
             if self._is_i2c_control_with_bmc():
                 self.stop(reason="BMC has taken over i2c bus")
                 self.exit.wait(30)
+                continue
+
+            if self._is_attention_fan_insertion_fail():
+                self.log.info("Attention fan insertion failed, trying to recover")
+                self._attention_fan_insertion_recovery()
                 continue
 
             if self._is_suspend():
