80ec51644fe1b953a3dcb365d54b9e5adb0131a1
Felix Radensky <fradensky@nvidia.com>
2025-09-09 20:41:52 +0300

    hw-mgmt: scripts: Ignore PDB EEPROM on SN58XX_LD platforms
    The EEPROM is empty and may be completely removed in future
    PDB revisions.
    
    Signed-off-by: Felix Radensky <fradensky@nvidia.com>

--- usr/usr/bin/hw-management-devtree.sh
+++ usr/usr/bin/hw-management-devtree.sh
@@ -566,7 +566,6 @@ declare -A sn58xxld_pwr_alternatives=( \
 				   ["mp29502_0"]="mp29502 0x60 7 pwr_conv1" \
 				   ["lm5066i_0"]="lm5066i 0x12 7 hotswap1" \
 				   ["mp5926_0"]="mp5926 0x12 7 hotswap1" \
-				   ["24c02_0"]="24c02 0x50 7 pdb_eeprom1" \
 				   ["tmp451_1"]="tmp451 0x4c 7 pdb_mosfet_amb1")
 
 # P*HaEaOfTk
