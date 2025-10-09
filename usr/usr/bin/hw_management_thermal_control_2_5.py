94836fd74eaa0b4c0b4eb5b29b1a838eaecff652
Oleksandr Shamray <oleksandrs@nvidia.com>
2025-10-09 18:25:51 +0300

    hw-mgmt: thermal: Fix module present status update and lcrit validation
    On cable removal TC should not check module temp value. But we have
    status update interval 60sec and i during this update cable will be
    removed - TC report sensor read error status.
    
    Fix: add module status update on each temperature reading
    
    Bug: 4657183
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw_management_thermal_control_2_5.py
+++ usr/usr/bin/hw_management_thermal_control_2_5.py
@@ -1774,12 +1774,13 @@ class thermal_module_sensor(system_device):
                 value = self.read_file_float(temp_read_file, self.scale)
                 self.log.debug("{} value:{}".format(self.name, value))
                 self.fread_err.handle_err(temp_read_file, reset=True)
-                # handle case if cable was replsed by the other cable with the sensor
-                if value != 0 and self.val_min == 0 and self.val_max == 0:
-                    self.log.info("{} refreshing min/max arttribures by the rule: val({}) min({}) max({})".format(self.name,
-                                                                                                                  value,
-                                                                                                                  self.val_min,
-                                                                                                                  self.val_max))
+                # handle case if cable was replaced by the other cable
+                # cable removed - value == 0 max != 0
+                # cable connected - value != 0 max == 0
+                if (value != 0 and self.val_max == 0) or (value == 0 and self.val_max != 0):
+                    self.log.info("{} refreshing min/max attributes by the rule: val({}) max({})".format(self.name,
+                                                                                                         value,
+                                                                                                         self.val_max))
                     self.refresh_attr()
 
                 if self.get_temp_support_status():
@@ -1787,14 +1788,14 @@ class thermal_module_sensor(system_device):
                         self.log.warn("{} value({}) >= hcrit({})".format(self.name,
                                                                          value,
                                                                          self.val_hcrit))
-                        self.fread_err.handle_err(self.file_input)
+                        self.fread_err.handle_err(temp_read_file)
                     elif self.val_lcrit is not None and value <= self.val_lcrit:
                         self.log.warn("{} value({}) <= lcrit({})".format(self.name,
                                                                          value,
                                                                          self.val_lcrit))
-                        self.fread_err.handle_err(self.file_input)
+                        self.fread_err.handle_err(temp_read_file)
                     else:
-                        self.fread_err.handle_err(self.file_input, reset=True)
+                        self.fread_err.handle_err(temp_read_file, reset=True)
                         self.update_value(value)
                         if self.value > self.val_max:
                             self.log.warn("{} value({}) >= ({})".format(self.name, self.value, self.val_max))
