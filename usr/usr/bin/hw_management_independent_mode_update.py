185d009742e16e0fd09a1934eef5b411cf14519c
Oleksandr Shamray <oleksandrs@nvidia.com>
2025-11-25 13:42:12 +0200

    hw-mgmt: scripts: Extend independent_mode_update:thermal_data_set_asic API
    1. Extend the independent_mode_update:thermal_data_set_asic API by adding
       new "asic1_*" attributes alongside the existing "asic_*" set.
       The "asic1_*" attributes duplicate the current "asic_*" values.
    
    2. Optimize and code cleanup
    
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- usr/usr/bin/hw_management_independent_mode_update.py
+++ usr/usr/bin/hw_management_independent_mode_update.py
@@ -38,13 +38,14 @@
 """Module providing functions for setting asic and module thermal data."""
 import os
 
-ERROR_READ_THERMAL_DATA = 254000
-
 BASE_PATH = "/var/run/hw-management"
 
 
 def get_asic_count():
-    """Function gets ASIC count from "{BASE_PATH}/config/asic_num."""
+    """
+    @summary: Function gets ASIC count from "{BASE_PATH}/config/asic_num."
+    @return: ASIC count if successful, False otherwise.
+    """
     asic_count_file = os.path.join(BASE_PATH, "config", "asic_num")
     if os.path.exists(asic_count_file):
         try:
@@ -59,7 +60,10 @@ def get_asic_count():
 
 
 def get_module_count():
-    """Function gets module count from "{BASE_PATH}/config/module_counter."""
+    """
+    @summary: Function gets module count from "{BASE_PATH}/config/module_counter."
+    @return: Module count if successful, False otherwise.
+    """
     module_count_file = os.path.join(BASE_PATH, "config", "module_counter")
     if os.path.exists(module_count_file):
         try:
@@ -74,7 +78,11 @@ def get_module_count():
 
 
 def check_asic_index(asic_index):
-    """Function checks asic index boundry."""
+    """
+    @summary: Function checks asic index boundary.
+    @param asic_index: ASIC index.
+    @return: True if asic index is valid, False otherwise.
+    """
     asic_count = get_asic_count()
     if asic_count is not False and 0 <= asic_index < asic_count:
         return True
@@ -83,16 +91,25 @@ def check_asic_index(asic_index):
 
 
 def check_module_index(asic_index, module_index):
-    """Function checks module index boundry."""
+    """
+    @summary: Function checks module index boundary.
+    @param asic_index: ASIC index.
+    @param module_index: Module index.
+    @return: True if module index is valid, False otherwise.
+    """
     module_count = get_module_count()
     if module_count is not False and 1 <= module_index <= module_count:
         return True
-    print(f"module_index {module_index}of asic {asic_index} is out of bound 1..n")
+    print(f"module_index {module_index} of asic {asic_index} is out of bound 1..n")
     return False
 
 
 def module_data_set_module_counter(module_counter):
-    """Function sets module counter."""
+    """
+    @summary: Function sets module counter.
+    @param module_counter: Module counter.
+    @return: True if module counter is set successfully, False otherwise.
+    """
     if module_counter < 0:
         print(f"Error: Could not set module count to {module_counter}")
         return False
@@ -101,43 +118,78 @@ def module_data_set_module_counter(module_counter):
     try:
         with open(module_count_file, 'w', encoding="utf-8") as f:
             f.write(str(module_counter))
-        return True  # Successfully set thermal data
+        return True  # Successfully set module counter
     except Exception as e:
         print(f"Error setting module counter: {str(e)}")
         return False
 
 
+def write_file_data(file_paths_value_dict):
+    """
+    @summary: Writes thermal data to the relevant files. Files/value passed as dictionary.
+    @param file_paths_value_dict: Dictionary of file names and values.
+    @return: True if all writes are successful, False otherwise.
+    """
+    try:
+        for fname, value in file_paths_value_dict.items():
+            f_name_full = os.path.join(BASE_PATH, "thermal", fname)
+            if value is not None:
+                with open(f_name_full, 'w', encoding="utf-8") as f:
+                    f.write("{}\n".format(value))
+    except Exception as e:
+        print(f"Error writing thermal data: {str(e)}")
+        return False
+    return True
+
+
+def remove_file_list(file_list):
+    """
+    @summary: Clears thermal data from the relevant files.
+    @param file_list: List of file names.
+    @return: True if all clears are successful, False otherwise.
+    """
+    try:
+        for file in file_list:
+            f_name_full = os.path.join(BASE_PATH, "thermal", file)
+            if os.path.exists(f_name_full):
+                os.remove(f_name_full)
+        return True
+    except Exception as e:
+        print(f"Error removing files: {file_list} {str(e)}")
+        return False
+
+
 def thermal_data_set_asic(asic_index, temperature, warning_threshold, critical_threshold, fault=0):
-    """Function sets asic data."""
+    """
+    @summary: Function sets asic data.
+    @param asic_index: ASIC index.
+    @param temperature: Temperature.
+    @param warning_threshold: Warning threshold.
+    @param critical_threshold: Critical threshold.
+    @param fault: Fault.
+    @return: True if all sets are successful, False otherwise.
+    """
     if not check_asic_index(asic_index):
         return False
 
+    file_paths_value_dict = {}
     # Define file paths based on asic_index
     if asic_index == 0:
-        temp_crit_file = os.path.join(BASE_PATH, "thermal", "asic_temp_crit")
-        temp_input_file = os.path.join(BASE_PATH, "thermal", "asic")
-        temp_emergency_file = os.path.join(BASE_PATH, "thermal", "asic_temp_emergency")
-        temp_fault_file = os.path.join(BASE_PATH, "thermal", "asic_temp_fault")
-    else:
-        temp_crit_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}_temp_crit")
-        temp_input_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}")
-        temp_emergency_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}_temp_emergency")
-        temp_fault_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}_temp_fault")
+        file_paths_value_dict.update({
+            "asic_temp_crit": critical_threshold,
+            "asic": temperature,
+            "asic_temp_emergency": warning_threshold,
+            "asic_temp_fault": fault
+        })
 
-    # Create the files if they don't exist and write the values
-    try:
-        with open(temp_crit_file, 'w', encoding="utf-8") as f:
-            f.write(str(critical_threshold))
-        with open(temp_input_file, 'w', encoding="utf-8") as f:
-            f.write(str(temperature))
-        with open(temp_emergency_file, 'w', encoding="utf-8") as f:
-            f.write(str(warning_threshold))
-        with open(temp_fault_file, 'w', encoding="utf-8") as f:
-            f.write(str(fault))
-        return True  # Successfully set thermal data
-    except Exception as e:
-        print(f"Error setting thermal data for ASIC {asic_index}: {str(e)}")
-        return False
+    file_paths_value_dict.update({
+        f"asic{asic_index + 1}_temp_crit": critical_threshold,
+        f"asic{asic_index + 1}": temperature,
+        f"asic{asic_index + 1}_temp_emergency": warning_threshold,
+        f"asic{asic_index + 1}_temp_fault": fault
+    })
+
+    return write_file_data(file_paths_value_dict)
 
 
 def thermal_data_set_module(asic_index,
@@ -146,46 +198,47 @@ def thermal_data_set_module(asic_index,
                             warning_threshold,
                             critical_threshold,
                             fault=0):
-    """Function sets module data."""
+    """
+    @summary: Function sets module data.
+    @param asic_index: ASIC index.
+    @param module_index: Module index.
+    @param temperature: Temperature.
+    @param warning_threshold: Warning threshold.
+    @param critical_threshold: Critical threshold.
+    @param fault: Fault.
+    @return: True if all sets are successful, False otherwise.
+    """
     if not check_asic_index(asic_index) or not check_module_index(asic_index, module_index):
         return False
 
-    # Define file paths
-    temp_crit_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_crit")
-    temp_input_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_input")
-    temp_emergency_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_emergency")
-    temp_fault_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_fault")
+    file_paths_value_dict = {
+        f"module{module_index}_temp_crit": critical_threshold,
+        f"module{module_index}_temp_input": temperature,
+        f"module{module_index}_temp_emergency": warning_threshold,
+        f"module{module_index}_temp_fault": fault
+    }
 
-    # Create the files if they don't exist and write the values
-    try:
-        with open(temp_crit_file, 'w', encoding="utf-8") as f:
-            f.write(str(critical_threshold))
-        with open(temp_input_file, 'w', encoding="utf-8") as f:
-            f.write(str(temperature))
-        with open(temp_emergency_file, 'w', encoding="utf-8") as f:
-            f.write(str(warning_threshold))
-        with open(temp_fault_file, 'w', encoding="utf-8") as f:
-            f.write(str(fault))
-        return True  # Successfully set thermal data for the module
-    except Exception as e:
-        print(f"Error setting thermal data for Module {module_index}: {str(e)}")
-        return False
-
-
-vendor_data_key_replace = {
-    "part_number": "PN",
-    "manufacturer": "Manufacturer"
-}
+    return write_file_data(file_paths_value_dict)
 
 
 def vendor_data_set_module(asic_index,
                            module_index,
                            vendor_info=None):
-    """Function sets module vendor data."""
+    """
+    @summary: Function sets module vendor data.
+    @param asic_index: ASIC index.
+    @param module_index: Module index.
+    @param vendor_info: Vendor information.
+    @return: True if all sets are successful, False otherwise.
+    """
     if not check_asic_index(asic_index) or not check_module_index(asic_index, module_index):
         return False
 
-    # Define file paths
+    vendor_data_key_replace = {
+        "part_number": "PN",
+        "manufacturer": "Manufacturer"
+    }
+
     vendor_file = os.path.join(BASE_PATH, "eeprom", f"module{module_index}_data")
     # Create the file if it doesn't exist and write the values
     try:
@@ -213,60 +266,61 @@ def vendor_data_set_module(asic_index,
 
 
 def thermal_data_clean_asic(asic_index):
-    """Function cleans asic data."""
+    """
+    @summary: Function cleans asic data.
+    @param asic_index: ASIC index.
+    @return: True if all cleans are successful, False otherwise.
+    """
     if not check_asic_index(asic_index):
         return False
 
-    # Define file paths based on asic_index
-    if asic_index == 0:
-        temp_crit_file = os.path.join(BASE_PATH, "thermal", "asic_temp_crit")
-        temp_input_file = os.path.join(BASE_PATH, "thermal", "asic")
-        temp_emergency_file = os.path.join(BASE_PATH, "thermal", "asic_temp_emergency")
-        temp_fault_file = os.path.join(BASE_PATH, "thermal", "asic_temp_fault")
-    else:
-        temp_crit_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}_temp_crit")
-        temp_input_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}")
-        temp_emergency_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}_temp_emergency")
-        temp_fault_file = os.path.join(BASE_PATH, "thermal", f"asic{asic_index + 1}_temp_fault")
+    file_paths_value_list = [
+        f"asic{asic_index + 1}_temp_crit",
+        f"asic{asic_index + 1}",
+        f"asic{asic_index + 1}_temp_emergency",
+        f"asic{asic_index + 1}_temp_fault"
+    ]
 
-    # Remove the files if they exist
-    try:
-        os.remove(temp_crit_file)
-        os.remove(temp_input_file)
-        os.remove(temp_emergency_file)
-        os.remove(temp_fault_file)
-        return True  # Successfully cleaned thermal data for the ASIC
-    except Exception as e:
-        print(f"Error cleaning thermal data for ASIC {asic_index}: {str(e)}")
-        return False
+    # For ASIC 0, also remove "asic_*" files
+    if asic_index == 0:
+        file_paths_value_list.extend([
+            "asic_temp_crit",
+            "asic",
+            "asic_temp_emergency",
+            "asic_temp_fault"
+        ])
+
+    return remove_file_list(file_paths_value_list)
 
 
 def thermal_data_clean_module(asic_index, module_index):
-    """Function cleans module data."""
+    """
+    @summary: Function cleans module data.
+    @param asic_index: ASIC index.
+    @param module_index: Module index.
+    @return: True if all cleans are successful, False otherwise.
+    """
     if not check_asic_index(asic_index) or not check_module_index(asic_index, module_index):
         return False
 
-    # Define file paths
-    temp_crit_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_crit")
-    temp_input_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_input")
-    temp_emergency_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_emergency")
-    temp_fault_file = os.path.join(BASE_PATH, "thermal", f"module{module_index}_temp_fault")
+    file_paths_value_list = [
+        f"module{module_index}_temp_crit",
+        f"module{module_index}_temp_input",
+        f"module{module_index}_temp_emergency",
+        f"module{module_index}_temp_fault"
+    ]
 
-    # Remove the files if they exist
-    try:
-        os.remove(temp_crit_file)
-        os.remove(temp_input_file)
-        os.remove(temp_emergency_file)
-        os.remove(temp_fault_file)
-        return True  # Successfully cleaned thermal data for the module
-    except Exception as e:
-        print(f"Error cleaning thermal data for Module {module_index}: {str(e)}")
-        return False
+    return remove_file_list(file_paths_value_list)
 
 
 def vendor_data_clear_module(asic_index,
                              module_index):
-    """Function cleans module vendor data."""
+    """
+    @summary: Function cleans module vendor data.
+    @param asic_index: ASIC index.
+    @param module_index: Module index.
+    @return: True if all cleans are successful, False otherwise.
+    """
     if not check_asic_index(asic_index) or not check_module_index(asic_index, module_index):
         return False
 
