0c5bddf7a630209c101ab04668694725e4feab25
Ciju Rajan K <crajank@nvidia.com>
2026-01-19 18:50:53 +0200

    hw-mgmt: scripts: Add support for N7170_LD virtual platform
    Signed-off-by: Ciju Rajan K <crajank@nvidia.com>

--- usr/usr/bin/hw-management.sh
+++ usr/usr/bin/hw-management.sh
@@ -3958,7 +3958,7 @@ case $ACTION in
 			exit 1
 		fi
 		# TEMPORARY hw-management mockup values for HI180 in simx
-		if check_simx && [ "$sku" == "HI180" -o "$sku" == "HI181" ]; then
+		if check_simx && [[ "$sku" == "HI180" || "$sku" == "HI181" || "$sku" == "HI194" ]]; then
 			tar -xzf /etc/hw-management-virtual/hwmgmt_$sku.tgz -C /var/run/
 			log_info "Created mock hw management tree, exiting."
 			exit 0
