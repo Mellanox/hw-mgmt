9bcbfc34c3d5a28710736436c3d37bb03d6a4c64
laviasaf <72394827+laviasaf@users.noreply.github.com>
2026-03-13 22:33:00 +0200

    Apply suggestions from code review
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- test.py
+++ test.py
@@ -29,5 +29,5 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
-def  my_function(x,y):
-   print "test"
+def my_function(x, y):
+    print("test")
