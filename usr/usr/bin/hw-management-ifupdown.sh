353e046ef74bd3e2ab60c9acb1b3581423c62f59
Felix Radensky <fradensky@nvidia.com>
2026-05-13 18:11:26 +0300

    hw-mgmt: scripts: Update usb0 hotplug handling
    The usb network interface naming was broken in kernel 6.1 between
    versions 6.1.115 and 6.1.132 by the following commit:
    d9edc3428c7: net: usb: usbnet: fix name regression
    As a result, the interface was named ethN instead of usb0.
    
    The problem was fixed in kernel 6.1.133 by the following commit:
    07f8bdce68b: net: usb: usbnet: restore usb%d name exception for local mac addresses
    
    This patch does the following:
    
     1. Adds new patch that applies the naming fix to affected kernel versions.
        The patch is skipped for NVOS that uses kernel version 6.1.94
     2. Updates udev rules to remove the redundant interface renaming and updates
        the matching criteria of the rule that runs configuration script.
     4. Updates configuration script comments and log messages
    
    Bug: 5020389
    
    Signed-off-by: Felix Radensky <fradensky@nvidia.com>

--- usr/usr/bin/hw-management-ifupdown.sh
+++ usr/usr/bin/hw-management-ifupdown.sh
@@ -38,26 +38,28 @@ source /usr/bin/hw-management-helpers.sh
 
 INTERFACE=$1
 
+# Check if interface parameter exists
 if [ -z "${INTERFACE}" ]; then
-	log_err "Missing interface parameter"
+	log_err "Interface parameter is missing"
 	exit 1
 fi
 
-if [ ! -e "/sys/class/net/${INTERFACE}" ]; then
-	log_info "Interface ${INTERFACE} is missing"
-	exit 0
-fi
-
+# Check if /etc/network/interfaces exists
 if [ ! -e /etc/network/interfaces ]; then
-	log_info "/etc/network/interfaces is missing"
-	exit 0
+	log_err "/etc/network/interfaces is missing"
+	exit 1
 fi
 
-AUTO=$(ifquery -l 2>/dev/null)
-HOTPLUG=$(ifquery -l --allow=hotplug 2>/dev/null)
+# Check if interface exists
+if [ ! -d "/sys/class/net/$INTERFACE" ]; then
+	log_err "Interface $INTERFACE does not exist"
+	exit 1
+fi
 
-if ! echo "$AUTO" "$HOTPLUG" | grep -q "${INTERFACE}"; then
-	exit 0
+# Check if interface is defined in /etc/network/interfaces
+if ! ifquery "$INTERFACE" >/dev/null 2>&1; then
+	log_err "Interface $INTERFACE is not defined in /etc/network/interfaces"
+	exit 1
 fi
 
 # Retry ifup to work around locking conflicts with Debian networking service.
@@ -73,10 +75,11 @@ for ((i=1; i<=MAX_RETRIES; i++)); do
 	fi
 
 	if [ "$i" -lt "$MAX_RETRIES" ]; then
-		log_info "Attempt $i to ifup ${INTERFACE} failed. Retrying in ${RETRY_DELAY} seconds..."
+		log_info "Unsuccessful attempt $i to ifup ${INTERFACE}. Retrying in ${RETRY_DELAY} seconds..."
 		sleep "${RETRY_DELAY}"
 	fi
 done
 
 log_err "Failed to ifup interface ${INTERFACE}"
 exit 1
+
