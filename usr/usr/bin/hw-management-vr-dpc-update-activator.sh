cec56caffe56a1c5cf5c390e4fea40ace132501f
Vasily Vityukov <vvityukov@nvidia.com>
2026-06-05 10:37:22 +0300

    hw-mgmt: scripts: Update copyright header in VR DPC MPS scripts
    Align SPDX copyright header to 2025-2026 in legacy MPS VR DPC tooling scripts
    (bulk-update, update-activator, per-device update) to match newer VR DPC scripts.

--- usr/usr/bin/hw-management-vr-dpc-update-activator.sh
+++ usr/usr/bin/hw-management-vr-dpc-update-activator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ################################################################################
 # SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
-# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
