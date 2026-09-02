59fe8e042970419524c6c0b7bb05420892fea5ee
Felix Radensky <fradensky@nvidia.com>
2026-09-02 10:18:38 +0300

    hw-mgmt: scripts: Fix whitespace problems in hw-management-devtree.sh
    Signed-off-by: Felix Radensky <fradensky@nvidia.com>

--- usr/usr/bin/hw-management-devtree.sh
+++ usr/usr/bin/hw-management-devtree.sh
@@ -201,22 +201,24 @@ declare -A msn27002_alternatives=( \
 	["24c512_0"]="24c512 0x51 8 vpd_info" \
 )
 
-declare -A msn4700_msn4600_alternatives=(["max11603_0"]="max11603 0x6d 5 swb_a2d" \
-					 ["xdpe12284_0"]="xdpe12284 0x62 5 voltmon1" \
-					 ["xdpe12284_1"]="xdpe12284 0x64 5 voltmon2" \
-					 ["xdpe12284_2"]="xdpe12284 0x66 5 voltmon3" \
-					 ["xdpe12284_3"]="xdpe12284 0x68 5 voltmon4" \
-					 ["xdpe12284_4"]="xdpe12284 0x6a 5 voltmon5" \
-					 ["xdpe12284_5"]="xdpe12284 0x6c 5 voltmon6" \
-					 ["xdpe12284_6"]="xdpe12284 0x6e 5 voltmon7" \
-					 ["mp2975_0"]="mp2975 0x62 5 voltmon1" \
-					 ["mp2975_1"]="mp2975 0x64 5 voltmon2" \
-					 ["mp2975_2"]="mp2975 0x66 5 voltmon3" \
-					 ["mp2975_3"]="mp2975 0x68 5 voltmon4" \
-					 ["mp2975_4"]="mp2975 0x6a 5 voltmon5" \
-					 ["mp2975_5"]="mp2975 0x6c 5 voltmon6" \
-					 ["mp2975_6"]="mp2975 0x6e 5 voltmon7" \
-					 ["24c32_0"]="24c32 0x51 8 vpd_info")
+declare -A msn4700_msn4600_alternatives=( \
+	["max11603_0"]="max11603 0x6d 5 swb_a2d" \
+	["xdpe12284_0"]="xdpe12284 0x62 5 voltmon1" \
+	["xdpe12284_1"]="xdpe12284 0x64 5 voltmon2" \
+	["xdpe12284_2"]="xdpe12284 0x66 5 voltmon3" \
+	["xdpe12284_3"]="xdpe12284 0x68 5 voltmon4" \
+	["xdpe12284_4"]="xdpe12284 0x6a 5 voltmon5" \
+	["xdpe12284_5"]="xdpe12284 0x6c 5 voltmon6" \
+	["xdpe12284_6"]="xdpe12284 0x6e 5 voltmon7" \
+	["mp2975_0"]="mp2975 0x62 5 voltmon1" \
+	["mp2975_1"]="mp2975 0x64 5 voltmon2" \
+	["mp2975_2"]="mp2975 0x66 5 voltmon3" \
+	["mp2975_3"]="mp2975 0x68 5 voltmon4" \
+	["mp2975_4"]="mp2975 0x6a 5 voltmon5" \
+	["mp2975_5"]="mp2975 0x6c 5 voltmon6" \
+	["mp2975_6"]="mp2975 0x6e 5 voltmon7" \
+	["24c32_0"]="24c32 0x51 8 vpd_info" \
+)
 
 declare -A mqm97xx_alternatives=( \
 	["mp2975_0"]="mp2975 0x62 5 voltmon1" \
